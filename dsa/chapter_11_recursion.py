f38666f90c53f2431803a2eb4c745742870fad02
Daniel Oschrin <dan@farmhq.com>
2024-12-02 10:24:36 -0800

    Solves triangular number problem

--- dsa/chapter_11_recursion.py
+++ dsa/chapter_11_recursion.py
@@ -1,9 +1,10 @@
-# Chapter 11 - Recursion
+# Chapter 11 - Recursion - page 181
 import unittest
 from typing import List, Union, Optional
 
 
 def count_characters_in_string_array(arr: List[str]):
+    """Problem 1"""
     # Base case
     # once we have counted the last character, arr[1:] will return an empty list, which will return 0
     if len(arr) == 0:
@@ -26,6 +27,7 @@ class CountCharactersInStringArrayTest(unittest.TestCase):
 
 
 def get_evens_in_number_array(arr: List[Union[int, float]], acc: Optional[List[int]] = None):
+    """Problem 2"""
     if acc is None:
         acc = []
     # Base case: We have no items in the array to check for evenness, so we return the accumulator
@@ -52,6 +54,31 @@ class GetEvensInNumberArrayTestCase(unittest.TestCase):
 
     def test_array_with_mixed_evens_and_odds(self):
         self.assertEqual(get_evens_in_number_array([5, 2, 300, 7]), [2, 300])
+
+
+def get_nth_triangular_number(n: int):
+    # Base case
+    if n == 0:
+        return 0
+    if n == 1:
+        return 1
+    # Recurse by adding n to the 'smaller problem', which would start with 1
+    # e.g. get_nth_triangular_number(2) would add 2 to get_nth_triangular number(1), which returns 1
+    return n + get_nth_triangular_number(n - 1)
+
+
+class GetNthTriangularNumberTestCase(unittest.TestCase):
+    def test_zero(self):
+        self.assertEqual(get_nth_triangular_number(0), 0)
+
+    def test_one(self):
+        self.assertEqual(get_nth_triangular_number(1), 1)
+
+    def test_n(self):
+        self.assertEqual(get_nth_triangular_number(2), 3)
+        self.assertEqual(get_nth_triangular_number(3), 6)
+        self.assertEqual(get_nth_triangular_number(4), 10)
+        self.assertEqual(get_nth_triangular_number(7), 28)
 
 
 if __name__ == '__main__':
