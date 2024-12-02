2118353179fb73411153b4a448e51e81e00947e2
Daniel Oschrin <dan@farmhq.com>
2024-12-02 11:00:12 -0800

    Solves chapter 11 problem 5

--- dsa/chapter_11_recursion.py
+++ dsa/chapter_11_recursion.py
@@ -1,4 +1,5 @@
 # Chapter 11 - Recursion - page 181
+import string
 import unittest
 from typing import List, Union, Optional
 
@@ -57,6 +58,7 @@ class GetEvensInNumberArrayTestCase(unittest.TestCase):
 
 
 def get_nth_triangular_number(n: int):
+    """Problem 3"""
     # Base case
     if n == 0:
         return 0
@@ -79,6 +81,60 @@ class GetNthTriangularNumberTestCase(unittest.TestCase):
         self.assertEqual(get_nth_triangular_number(3), 6)
         self.assertEqual(get_nth_triangular_number(4), 10)
         self.assertEqual(get_nth_triangular_number(7), 28)
+
+
+def get_first_index_of_x(target: str):
+    """Problem 4"""
+    # base case - we found it
+    if target[0] == 'x':
+        return 0
+    # We add 1 to the index because we have 'moved' further into the string
+    return 1 + get_first_index_of_x(target[1:])
+
+
+class GetFirstIndexOfXTestCase(unittest.TestCase):
+    # def test_empty_string(self):
+    #     self.assertEqual(get_first_index_of_x(''), -1)
+
+    # def test_string_with_no_x(self):
+    #     self.assertEqual(get_first_index_of_x('abc'), -1)
+
+    def test_string_with_x(self):
+        self.assertEqual(get_first_index_of_x('x'), 0)
+        self.assertEqual(get_first_index_of_x('xxx'), 0)
+        self.assertEqual(get_first_index_of_x('abc x'), 4)
+        self.assertEqual(get_first_index_of_x(string.ascii_lowercase), 23)
+
+
+def find_unique_paths(n_rows: int, n_columns: int):
+    """Problem 5"""
+    if n_rows == 0 or n_columns == 0:
+        return 0
+    if n_rows == 1 and n_columns == 1:
+        return 1
+
+    # We are breaking the possibilities down into the sum of the two smaller grids we could create
+    # by reducing the dimensions by one.
+    return find_unique_paths(n_rows - 1, n_columns) + find_unique_paths(n_rows, n_columns - 1)
+
+
+class FindUniquePathsTestCase(unittest.TestCase):
+    def test_one_by_one_grid(self):
+        self.assertEqual(find_unique_paths(1, 1), 1)
+
+    def test_one_by_two_grid(self):
+        self.assertEqual(find_unique_paths(1, 2), 1)
+        self.assertEqual(find_unique_paths(2, 1), 1)
+
+    def test_one_by_long_grid(self):
+        self.assertEqual(find_unique_paths(1, 100), 1)
+
+    def test_two_by_two_grid(self):
+        self.assertEqual(find_unique_paths(2, 2), 2)
+
+    def test_complex_grid(self):
+        self.assertEqual(find_unique_paths(3, 3), 6)
+        self.assertEqual(find_unique_paths(3, 7), 28)
 
 
 if __name__ == '__main__':
